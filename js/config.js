96680febd25d348ee4f5a7fdb048697a9ea412e1
tndduc <76874304+tndduc@users.noreply.github.com>
2023-06-14 04:44:18 +0700

    Update config.js
    Signed-off-by: tndduc <76874304+tndduc@users.noreply.github.com>

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-var myVariable = 'localhost';
-// var gitlink = 'https://tndduc.github.io/app_doctor/';
-var gitlink = 'http://127.0.0.1:5500/ducker/front_end';
+var myVariable = '192.168.1.206';
+var gitlink = 'https://tndduc.github.io/app_doctor/';
+// var gitlink = 'http://127.0.0.1:5500/ducker/front_end';
